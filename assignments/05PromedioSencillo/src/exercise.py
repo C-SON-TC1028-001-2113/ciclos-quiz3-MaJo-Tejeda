7e4423e504f3e3870be8ea4997806cb6368a711e
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-09-03 09:20:17 -0700

    Update exercise.py

--- assignments/05PromedioSencillo/src/exercise.py
+++ assignments/05PromedioSencillo/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+ n = int(input('Ingresa la cantidad de valores que quieres promediar'))
+
+ print("Ingresa los valores)
+    while n < 0:
+    x = int 
+    x += x 
+        break  
+        total = x/n 
+        print(total) 
+
+    
 
 if __name__=='__main__':
     main()
